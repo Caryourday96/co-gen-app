c25641d9da758fe518be582168d7d49b4ddee2e5
Caryourday <adetkay@gmail.com>
2019-03-22 15:33:46 -0400

    Added CarbonDioxide Page
    Generated the mainpage for carbondioxide

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,8 @@ const routes: Routes = [
   {
     path: 'list',
     loadChildren: './list/list.module#ListPageModule'
-  }
+  },
+  { path: 'carbon', loadChildren: './carbon/carbon.module#CarbonPageModule' }
 ];
 
 @NgModule({
