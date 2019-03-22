b7d49bd90528d1c848a1ba07f60ec06f2e9a3159
Caryourday <adetkay@gmail.com>
2019-03-22 17:51:46 -0400

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,13 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+
 import { environment } from 'src/environments/environment';
 import { AuthenticationService } from './services/authentication.service';
 import { AngularFireAuthModule } from '@angular/fire/auth';
  
 import * as firebase from '@firebase/app';
+
 
 import '@firebase/firestore'
 
