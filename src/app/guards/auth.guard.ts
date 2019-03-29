20fd1d6b1dd8cff79df6c2cd49d583334246202f
Caryourday <adetkay@gmail.com>
2019-03-28 21:31:51 -0400

    Added Guard to App
    Added auth to app, so only home can be shown until user is logged in.

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,10 +1,32 @@
-import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Injectable, NgZone } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthenticationService } from '../services/authentication.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements  {
-  
-}
+export class AuthGuard implements  CanActivate {
+
+  constructor(
+    private auth: AuthenticationService,
+    private router: Router,
+    private ngZone: NgZone
+  ){}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+ 
+    let userAuthenticated = false; // Get the current authentication state from a Service!
+ 
+    if (userAuthenticated) {
+      return true;
+    } else {
+             this.ngZone.run(() => {
+         this.router.navigateByUrl('/login');
+       });
+      return false;
+    }
+  }
+ }
