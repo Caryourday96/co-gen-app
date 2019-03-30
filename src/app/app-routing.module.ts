92f4e13f1a0ece5dd5c0f8e9d01748c0076935eb
vithu <srib0001@algonquinlive.com>
2019-03-29 20:39:58 -0400

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,52 +1,6 @@
-<<<<<<< HEAD
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './guards/auth.guard';
-=======
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-  {
-    path: 'home',
-    loadChildren: './home/home.module#HomePageModule'
-  },
-  {
-    path: 'list',
-    loadChildren: './list/list.module#ListPageModule'
-  },
-  { 
-    path: 'carbon', 
-  loadChildren: './carbon/carbon.module#CarbonPageModule' 
-  },
-  { 
-    path: 'electricity', 
-  loadChildren: './electricity/electricity.module#ElectricityPageModule' 
-  },
-
-  { 
-    path: 'login', 
-  loadChildren: './login/login.module#LoginPageModule' 
-  },
-
-  { 
-    path: 'signup', 
-    loadChildren: './signup/signup.module#SignupPageModule' 
-  },
-  { path: 'e-daily',
-   loadChildren: './e-daily/e-daily.module#EDailyPageModule' },
-
-
-  { path: 'e-weekly', 
-  loadChildren: './e-weekly/e-weekly.module#EWeeklyPageModule' },  { path: 'co2-daily', loadChildren: './co2-daily/co2-daily.module#CO2DailyPageModule' },
-  { path: 'co2-weekly', loadChildren: './co2-weekly/co2-weekly.module#CO2WeeklyPageModule' },
-  { path: 'co2-monthly', loadChildren: './co2-monthly/co2-monthly.module#Co2MonthlyPageModule' }
->>>>>>> Vithura
 
 const routes: Routes = [
   {
